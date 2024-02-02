beb8daf8301a89c60ad08824e390edbac59a6446
PatilKrantiDipak <krantidpatil2601@gmail.com>
2024-02-02 17:09:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <div>
+            <ul>
+              <li>MOBILE-FRIENDLY</li>
+              <li>MODERN & PROFESSIONAL</li>
+              <li>TECHNICAL INTEGRATION</li>
+              <li>COPYWRITING</li>
+              <li>ANALYTICS</li>
+              <li>BRANDING</li>
+              <li>UX DESIGN</li>
+              <li>DEVELOPMENT</li>
+              <li>PHOTOGRAPHY</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col-md-8">
+          <div>
+            <p>
+              <span style={{ color: 'rgb(79, 33, 122)', fontWeight: 'bold', fontSize: '25px' }}>WEBSITE</span>
+              <span style={{ color: 'white', fontWeight: 'bold', fontSize: '25px' }}>PACKAGE</span>
+            </p>
+            <div className="text2">
+              <p>As you decide to get a new website, there are a few things that you need to consider. This starts with understanding the level of design and aesthetic value you are looking for. Do you desire a custom design or can you use an existing template? What is the objective of the website - lead generation, information sharing, or both? There are many such aspects that you need to brainstorm before designing a new website and the process can be a little overwhelming.</p>
+              <p>We are here with our hands-on experience to build websites that are data-driven and result-oriented. We pay attention to the user experience and create a journey that converts prospects into customers. Your website leaves an impression on the visitor, and we ensure that the impression is long-lasting.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
